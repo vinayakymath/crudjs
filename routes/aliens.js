668fb4b546628ed9306c2b27839134d5ebcaaa7f
vinayak <vinayakcy123@gmail.com>
2020-11-28 11:10:14 +0530

    router updated

--- routes/aliens.js
+++ routes/aliens.js
@@ -11,7 +11,16 @@ router.get('/', async(req, res)=>{
     }
 })
 
-router.post('/', async(req,res) => {
+router.get('/:id', async (req, res) => {
+    try {
+        const alien = await Alien.findById(req.params.id)
+        res.json(alien)
+    } catch (err) {
+        res.send('error' + err)
+    }
+})
+
+router.post('/', async(req, res) => {
     const alien = new Alien({
         name: req.body.name,
         tech: req.body.tech,
@@ -22,7 +31,19 @@ router.post('/', async(req,res) => {
         const a1 = await alien.save()
         res.json(a1)
     }catch(err){
-        res.send('error')
+        res.send('error' + err)
+    }
+})
+
+//patch
+router.patch('/:id', async(req,res) => {
+    try{
+        const alien = await Alien.findById(req.params.id)
+        alien.sub = req.body.sub
+        const a1 = await alien.save()
+        res.json(a1)
+    }catch(err){
+        res.send('error' + err)
     }
 })
 
